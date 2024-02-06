3103399307f74735a910a54d0705c23d460a8443
ramanasai <kingofkingsairamana@gmail.com>
2024-02-06 08:38:48 +0530

    major changes

--- app.py
+++ app.py
@@ -24,11 +24,15 @@ async def create_upload_file(file: UploadFile = File(...)):
     ol.save_image(file, file_location)
     pil_image = Image.open(file_location)
     img_base64 = ol.convert_to_base64(pil_image)
+    global llm_with_image_context
     llm_with_image_context = model_llava.bind(images=[img_base64])
   except Exception as e:
     return {"error": str(e)}
   return llm_with_image_context.invoke("What does the image contain?")
 
+@app.get("/input-text"
+async def input_text(text: str):
+  return llm_with_image_context.invoke(text)
 
 if __name__ == "__main__":
   ol = OllamaLlava()
--- ui.py
+++ ui.py
@@ -2,23 +2,39 @@ import streamlit as st
 from dotenv import load_dotenv
 import requests
 
-def main():
-  st.title("OllamaLlava")
-  st.write("Welcome to OllamaLlava, the image recognition app")
 
-  file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
+def image_upload():
+  with st.sidebar:
+    file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
-  if st.button("Submit"):
     if file is not None:
-      url_upload = "http://localhost:8098/uploadfile"
-      files = {"file": file}
-      response = requests.post(url_upload, files=files)
-      st.write(response.json())
-
+        try:
+            files = {"file": file}
+            response = requests.post(url_upload, files=files)
+        except Exception as e:
+            st.write(str(e))
     else:
-      st.write("Please upload an image")
+        st.write("Please upload an image")
+
+
+def main():
+    st.title("OllamaLlava")
+    st.write("Welcome to OllamaLlava, the image recognition app")
+
+    image_upload()
+
+    messages = st.container(height=300)
+    prompt = st.text_input("Say something")
+    if st.button("Send"):
+        if prompt:
+            messages.chat_message("user").write(prompt)
+            messages.chat_message("assistant").write(
+                f"Echo: {requests.get(url_input_text + prompt).json()}"
+            )
 
 
 if __name__ == "__main__":
-  load_dotenv()
-  main()
+    load_dotenv()
+    url_upload = "http://127.0.0.1:8099/uploadfile"
+    url_input_text = "http://127.0.0.1:8099/input-text?text="
+    main()
